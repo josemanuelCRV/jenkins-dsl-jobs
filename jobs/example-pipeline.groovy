bc227f629408fc04d773da10d8b31a9c61001e51
Jose Manuel Calvo <josema.calvo@servexternos.gruposantander.com>
2020-02-24 01:05:08 +0100

    first commit

---
+++ jobs/example-pipeline.groovy
@@ -0,0 +1,67 @@
+#!groovy
+
+multibranchPipelineJob('example') {
+
+    triggers {
+        periodic(1)
+    }
+
+    branchSources {
+        git {
+            remote('https://github.com/josemanuelCRV/octo-repo.git')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+project_name = "jenkins-pipeline-demo"
+repo = "https://github.com/josemanuelCRV/octo-repo.git"
+repo_name = "octo-repo"
+
+multibranchPipelineJob('example') {
+    definition {
+        triggers {
+            scm('H/1 * * * *')
+        }
+
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+        
+    }
+
+
+
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            credentialsId('github-ci')
+            includes('JENKINS-*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
